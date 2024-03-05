e73bc3e17587cb482f879fca3f8af2aa76b329fe
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-03-05 17:15:18 +0000

    Use original caremu audio
    Using original caremu audio,
    since extended was used for Android 12.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -6,12 +6,14 @@ PRODUCT_COPY_FILES += \
     device/epam/aosp-xenvm-trout/shared/config/ueventd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.xenvm_trout.rc
 
 LOCAL_AUDIO_PROPERTIES ?= \
-    ro.hardware.audio.primary=caremu-extended \
+    ro.hardware.audio.primary=caremu \
     ro.vendor.caremu.audiohal.out_period_ms=40 \
     ro.vendor.caremu.audiohal.in_period_ms=40 \
 
 LOCAL_VHAL_PRODUCT_PACKAGE ?= android.hardware.automotive.vehicle@2.0-virtualization-service-extended
 
-PRODUCT_PACKAGES += audio.primary.caremu-extended
+PRODUCT_PACKAGES += audio.primary.caremu
+
+
 
 PRODUCT_PACKAGE_OVERLAYS += device/epam/aosp-xenvm-trout/overlay
