a41afc1940b3440a0ab0cece90d86cfcc1947ea6
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-03-06 18:09:05 +0000

    Use custom fstab to be able to mount dlkm partition
    In Android 14 we will use dlkm partition for vendor
    kernel modules.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -15,5 +15,11 @@ LOCAL_VHAL_PRODUCT_PACKAGE ?= android.hardware.automotive.vehicle@2.0-virtualiza
 PRODUCT_PACKAGES += audio.primary.caremu
 
 
+PRODUCT_COPY_FILES += \
+    device/epam/aosp-xenvm-trout/shared/config/fstab.trout_xenvm:$(TARGET_COPY_OUT_RAMDISK)/fstab.trout_xenvm \
+    device/epam/aosp-xenvm-trout/shared/config/fstab.trout_xenvm:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.trout_xenvm \
+    device/epam/aosp-xenvm-trout/shared/config/fstab.trout_xenvm:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.trout_xenvm \
+    device/epam/aosp-xenvm-trout/shared/config/fstab.trout_xenvm:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.trout_xenvm \
+    device/epam/aosp-xenvm-trout/shared/config/fstab.trout_xenvm:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.trout_xenvm
 
 PRODUCT_PACKAGE_OVERLAYS += device/epam/aosp-xenvm-trout/overlay
