33d3044bc2050f4d882a43bafd1c1087cac29bb2
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-03-06 18:13:16 +0000

    do not use custom rc files
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- aosp_xenvm_trout_common.mk
+++ aosp_xenvm_trout_common.mk
@@ -1,9 +1,6 @@
 BOARD_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy
 
-PRODUCT_COPY_FILES += \
-    device/epam/aosp-xenvm-trout/shared/config/init.vendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xenvm_trout.rc \
-    device/epam/aosp-xenvm-trout/shared/config/init.recovery.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.xenvm_trout.rc \
-    device/epam/aosp-xenvm-trout/shared/config/ueventd.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.xenvm_trout.rc
+
 
 LOCAL_AUDIO_PROPERTIES ?= \
     ro.hardware.audio.primary=caremu \
