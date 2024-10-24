04ff75d5dbf0f0d87fc5f5a869c8cebec50d7fe7
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2024-10-24 13:35:03 +0000

    Move androidboot* and specific properties to bootconfig
    The proper way for Android is to keep all androidboot properties
    in the bootconfig. Adding also specific properties to it.
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>
    Acked-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

--- xenvm_trout_arm64/BoardConfig.mk
+++ xenvm_trout_arm64/BoardConfig.mk
@@ -1,4 +1,3 @@
-BOARD_KERNEL_CMDLINE += androidboot.fstab_name=fstab androidboot.fstab_suffix=trout_xenvm
 
 TARGET_USERDATAIMAGE_PARTITION_SIZE := 7516192768 # 7 GB
 
@@ -7,3 +6,24 @@ TARGET_USERDATAIMAGE_PARTITION_SIZE := 7516192768 # 7 GB
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648 # 2 GB
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/epam/aosp-xenvm-trout/sepolicy/private
+
+BOARD_BOOTCONFIG := androidboot.fstab_name=fstab
+BOARD_BOOTCONFIG += androidboot.fstab_suffix=trout_xenvm
+BOARD_BOOTCONFIG += androidboot.vendor.apex.com.android.hardware.keymint=com.android.hardware.keymint.rust_nonsecure
+BOARD_BOOTCONFIG += androidboot.vendor.apex.com.android.hardware.gatekeeper=com.android.hardware.gatekeeper.nonsecure
+BOARD_BOOTCONFIG += androidboot.vendor.vehiclehal.server.cid=2
+BOARD_BOOTCONFIG += androidboot.vendor.vehiclehal.server.port=9210
+BOARD_BOOTCONFIG += androidboot.vendor.vehiclehal.server.psf=/data/data/power.file
+BOARD_BOOTCONFIG += androidboot.vendor.vehiclehal.server.pss=/data/data/power.socket
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
+BOARD_BOOTCONFIG += androidboot.android_dt_dir=/proc/device-tree/firmware#1/android/
+BOARD_BOOTCONFIG += kernel.vmw_vsock_virtio_transport_common.virtio_transport_max_vsock_pkt_buf_size=16384
+BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
+BOARD_BOOTCONFIG += androidboot.enable_bootanimation=0
+BOARD_BOOTCONFIG += androidboot.lcd_density=160
+BOARD_BOOTCONFIG += androidboot.hardware.hwcomposer=ranchu
+BOARD_BOOTCONFIG += androidboot.hardware.hwcomposer.mode=client
+BOARD_BOOTCONFIG += androidboot.hardware.hwcomposer.display_finder_mode=drm
+BOARD_BOOTCONFIG += androidboot.hardware.gralloc=minigbm
+BOARD_BOOTCONFIG += androidboot.hardware.egl=mesa androidboot.boot_devices=33000000.pcie
+BOARD_BOOTCONFIG += androidboot.openthread_node_id=1
