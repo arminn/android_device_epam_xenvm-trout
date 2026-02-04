6d306a5b62c1870e266a85fb44b668205448b53b
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2026-02-04 12:47:22 +0000

    Revert "Revert "Use default vehicle HAL implementation""
    This reverts commit ba4a7327db8aae537d8c2c41ab5e3f1e98664aad.

--- aosp_xenvm_trout_arm64.mk
+++ aosp_xenvm_trout_arm64.mk
@@ -30,7 +30,7 @@ PRODUCT_VENDOR_PROPERTIES += ro.hardware.vulkan=powervr
 
 # To override VHAL, declare LOCAL_VHAL_PRODUCT_PACKAGE
 # prior to device/google/trout/aosp_trout_arm64.mk include
-LOCAL_VHAL_PRODUCT_PACKAGE = android.hardware.automotive.vehicle@2.0-virtualization-service
+LOCAL_VHAL_PRODUCT_PACKAGE = android.hardware.automotive.vehicle@2.0-default-service
 
 DEVICE_VIRTWIFI_PORT := eth0
 
